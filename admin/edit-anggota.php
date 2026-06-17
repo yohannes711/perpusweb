b3124bc15895fd03f55d2730d2aef96bc3fa7e57
Yohannes <130846266+yohannes711@users.noreply.github.com>
2026-06-17 14:14:31 +0700

    Initial commit

---
+++ admin/edit-anggota.php
@@ -0,0 +1,292 @@
+<?php
+session_start();
+if (empty($_SESSION['username'])) {
+    header('location:../index.php');
+} else {
+    include "../conn.php";
+?>
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <meta charset="UTF-8">
+        <title>PerpusE-book</title>
+        <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+        <meta name="description" content="Hakko Bio Richard">
+        <meta name="keywords" content="Perpus, Website, Aplikasi, Perpustakaan, Online">
+        <!-- bootstrap 3.0.2 -->
+        <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <!-- font Awesome -->
+        <link href="../css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <!-- Ionicons -->
+        <link href="../css/ionicons.min.css" rel="stylesheet" type="text/css" />
+        <!-- Morris chart -->
+        <link href="../css/morris/morris.css" rel="stylesheet" type="text/css" />
+        <!-- jvectormap -->
+        <link href="../css/jvectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />
+        <!-- Date Picker -->
+        <link href="../css/datepicker/datepicker3.css" rel="stylesheet" type="text/css" />
+        <!-- fullCalendar -->
+        <!-- <link href="css/fullcalendar/fullcalendar.css" rel="stylesheet" type="text/css" /> -->
+        <!-- Daterange picker -->
+        <link href="../css/daterangepicker/daterangepicker-bs3.css" rel="stylesheet" type="text/css" />
+        <!-- iCheck for checkboxes and radio inputs -->
+        <link href="../css/iCheck/all.css" rel="stylesheet" type="text/css" />
+        <!-- bootstrap wysihtml5 - text editor -->
+        <!-- <link href="css/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css" rel="stylesheet" type="text/css" /> -->
+        <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+        <!-- Theme style -->
+        <link href="../css/style.css" rel="stylesheet" type="text/css" />
+
+        <style type="text/css">
+
+        </style>
+    </head>
+
+    <body class="skin-black">
+        <!-- header logo: style can be found in header.less -->
+        <header class="header">
+            <a href="index.php" class="logo">
+                PerpusE-book
+            </a>
+            <!-- Header Navbar: style can be found in header.less -->
+            <nav class="navbar navbar-static-top" role="navigation">
+                <!-- Sidebar toggle button-->
+                <a href="#" class="navbar-btn sidebar-toggle" data-toggle="offcanvas" role="button">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </a>
+                <div class="navbar-right">
+                    <ul class="nav navbar-nav">
+
+                        <!-- User Account: style can be found in dropdown.less -->
+                        <li class="dropdown user user-menu">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="fa fa-user"></i>
+                                <span><?php echo $_SESSION['fullname']; ?> <i class="caret"></i></span>
+                            </a>
+                            <ul class="dropdown-menu dropdown-custom dropdown-menu-right">
+                                <li class="dropdown-header text-center">Account</li>
+
+                                <li>
+                                    <a href="detail-admin.php?hal=edit&kd=<?php echo $_SESSION['user_id']; ?>">
+                                        <i class="fa fa-user fa-fw pull-right"></i>
+                                        Profile
+                                    </a>
+                                    <a href="admin.php">
+                                        <i class="fa fa-cog fa-fw pull-right"></i>
+                                        Settings
+                                    </a>
+                                </li>
+
+                                <li class="divider"></li>
+
+                                <li>
+                                    <a href="../logout.php"><i class="fa fa-ban fa-fw pull-right"></i> Logout</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <?php
+        $timeout = 10; // Set timeout minutes
+        $logout_redirect_url = "../login.html"; // Set logout URL
+
+        $timeout = $timeout * 60; // Converts minutes to seconds
+        if (isset($_SESSION['start_time'])) {
+            $elapsed_time = time() - $_SESSION['start_time'];
+            if ($elapsed_time >= $timeout) {
+                session_destroy();
+                echo "<script>alert('Session Anda Telah Habis!'); window.location = '$logout_redirect_url'</script>";
+            }
+        }
+        $_SESSION['start_time'] = time();
+        ?>
+    <?php } ?>
+    <div class="wrapper row-offcanvas row-offcanvas-left">
+        <!-- Left side column. contains the logo and sidebar -->
+        <aside class="left-side sidebar-offcanvas">
+            <!-- sidebar: style can be found in sidebar.less -->
+            <section class="sidebar">
+                <!-- Sidebar user panel -->
+                <div class="user-panel">
+                    <div>
+                        <center><img src="<?php echo $_SESSION['gambar']; ?>" height="80" width="80" class="img-circle" alt="User Image" style="border: 3px solid white;" /></center>
+                    </div>
+                    <div class="info">
+                        <center>
+                            <p><?php echo $_SESSION['fullname']; ?></p>
+                        </center>
+
+                    </div>
+                </div>
+               
+                <?php include "menu.php"; ?>
+            </section>
+            <!-- /.sidebar -->
+        </aside>
+
+        <aside class="right-side">
+
+            <!-- Main content -->
+            <section class="content">
+
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="panel">
+                            <header class="panel-heading">
+                                <b>Edit Anggota</b>
+
+                            </header>
+                            <!-- <div class="box-header"> -->
+                            <!-- <h3 class="box-title">Responsive Hover Table</h3> -->
+                            <?php
+                            $query = mysqli_query($conn, "SELECT * FROM data_anggota WHERE id='$_GET[kd]'");
+                            $data  = mysqli_fetch_array($query);
+                            ?>
+                            <!-- </div> -->
+                            <div class="panel-body">
+                                <form class="form-horizontal style-form" style="margin-top: 20px;" action="update-anggota.php" method="post" enctype="multipart/form-data" name="form1" id="form1">
+                                    <div class="form-group">
+                                        <label class="col-sm-2 col-sm-2 control-label">ID Anggota</label>
+                                        <div class="col-sm-8">
+                                            <input name="id" type="text" id="id" class="form-control" placeholder="Tidak perlu di isi" value="<?php echo $data['id']; ?>" autofocus="on" readonly="readonly" />
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 col-sm-2 control-label">Email</label>
+                                        <div class="col-sm-8">
+                                            <input name="email" type="text" id="email" class="form-control" placeholder="Ex : xxx.@gmail.com" value="<?php echo $data['email']; ?>" required />
+                                            <!--<span class="help-block">A block of help text that breaks onto a new line and may extend beyond one line.</span>-->
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 col-sm-2 control-label">Nama</label>
+                                        <div class="col-sm-8">
+                                            <input name="nama" type="text" id="nama" class="form-control" placeholder="Nama" value="<?php echo $data['nama']; ?>" required />
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 col-sm-2 control-label">Username</label>
+                                        <div class="col-sm-8">
+                                            <input name="username" type="text" id="username" class="form-control" placeholder="Username" value="<?php echo $data['username']; ?>" required />
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 col-sm-2 control-label">Password</label>
+                                        <div class="col-sm-8">
+                                            <input name="password" type="text" id="password" class="form-control" placeholder="Password" value="<?php echo $data['password']; ?>" required />
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 col-sm-2 control-label">Jenis Kelamin</label>
+                                        <div class="col-sm-4">
+                                            <select class="form-control" name="jk" id="jk">
+                                                <option> <?php echo $data['jk']; ?></option>
+                                                <option value="L"> Laki - Laki</option>
+                                                <option value="P"> Perempuan</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 col-sm-2 control-label">Usia</label>
+                                        <div class="col-sm-8">
+                                            <input name="kelas" class="form-control" id="kelas" type="text" placeholder="Usia" value="<?php echo $data['kelas']; ?>" required />
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 col-sm-2 control-label">Tempat Tanggal Lahir</label>
+                                        <div class="col-sm-8">
+                                            <input name="ttl" class="form-control" id="ttl" type="text" placeholder="Tempat, DD MM YY" value="<?php echo $data['ttl']; ?>" required />
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 col-sm-2 control-label">Alamat</label>
+                                        <div class="col-sm-8">
+                                            <input name="alamat" class="form-control" id="alamat" type="text" placeholder="Alamat" value="<?php echo $data['alamat']; ?>" required />
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 col-sm-2 control-label">Foto</label>
+                                        <div class="col-sm-8">
+                                            <span class="help-block">Silahkan memilih foto untuk update atau mengedit data, tekan tombol batal untuk kembali.</span>
+                                            <img src="<?php echo $data['foto']; ?>" height="250" width="250" alt="Foto Anggota" style="margin-bottom: 10px;" /><br />
+                                            <input name="nama_file" id="nama_file" type="file" />
+                                        </div>
+                                    </div>
+                                    <div class="form-group" style="margin-bottom: 20px;">
+                                        <label class="col-sm-2 col-sm-2 control-label"></label>
+                                        <div class="col-sm-8">
+                                            <input type="submit" value="Simpan" class="btn btn-sm btn-primary" />&nbsp;
+                                            <a href="anggota.php" class="btn btn-sm btn-danger">Batal </a>
+                                        </div>
+                                    </div>
+                                    <div style="margin-top: 20px;"></div>
+                                </form>
+                            </div>
+                        </div><!-- /.box -->
+                    </div>
+                </div>
+                <!-- row end -->
+            </section><!-- /.content -->
+            
+        </aside><!-- /.right-side -->
+
+    </div><!-- ./wrapper -->
+
+
+    <!-- jQuery 2.0.2 -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+    <script src="../js/jquery.min.js" type="text/javascript"></script>
+
+    <!-- jQuery UI 1.10.3 -->
+    <script src="../js/jquery-ui-1.10.3.min.js" type="text/javascript"></script>
+    <!-- Bootstrap -->
+    <script src="../js/bootstrap.min.js" type="text/javascript"></script>
+    <!-- daterangepicker -->
+    <script src="../js/plugins/daterangepicker/daterangepicker.js" type="text/javascript"></script>
+
+    <script src="../js/plugins/chart.js" type="text/javascript"></script>
+
+    
+    <script src="../js/plugins/iCheck/icheck.min.js" type="text/javascript"></script>
+    <!-- calendar -->
+    <script src="../js/plugins/fullcalendar/fullcalendar.js" type="text/javascript"></script>
+
+    <!-- Director App -->
+    <script src="../js/Director/app.js" type="text/javascript"></script>
+
+    <!-- Director dashboard demo (This is only for demo purposes) -->
+    <script src="../js/Director/dashboard.js" type="text/javascript"></script>
+
+    <!-- Director for demo purposes -->
+    <script type="text/javascript">
+        $('input').on('ifChecked', function(event) {
+            $(this).parents('li').addClass("task-done");
+            console.log('ok');
+        });
+        $('input').on('ifUnchecked', function(event) {
+            $(this).parents('li').removeClass("task-done");
+            console.log('not');
+        });
+    </script>
+    <script>
+        $('#noti-box').slimScroll({
+            height: '400px',
+            size: '5px',
+            BorderRadius: '5px'
+        });
+
+        $('input[type="checkbox"].flat-grey, input[type="radio"].flat-grey').iCheck({
+            checkboxClass: 'icheckbox_flat-grey',
+            radioClass: 'iradio_flat-grey'
+        });
+    </script>
+    
+    </body>
+
+    </html>
---
+++ admin/input-admin.php
@@ -0,0 +1,248 @@
+<?php
+session_start();
+if (empty($_SESSION['username'])) {
+    header('location:../index.php');
+} else {
+    include "../conn.php";
+?>
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <meta charset="UTF-8">
+        <title>PerpusE-book</title>
+        <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+        <meta name="description" content="Hakko Bio Richard">
+        <meta name="keywords" content="Perpus, Website, Aplikasi, Perpustakaan, Online">
+        <!-- bootstrap 3.0.2 -->
+        <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <!-- font Awesome -->
+        <link href="../css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <!-- Ionicons -->
+        <link href="../css/ionicons.min.css" rel="stylesheet" type="text/css" />
+        <!-- Morris chart -->
+        <link href="../css/morris/morris.css" rel="stylesheet" type="text/css" />
+        <!-- jvectormap -->
+        <link href="../css/jvectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />
+        <!-- Date Picker -->
+        <link href="../css/datepicker/datepicker3.css" rel="stylesheet" type="text/css" />
+        <!-- fullCalendar -->
+        <!-- <link href="css/fullcalendar/fullcalendar.css" rel="stylesheet" type="text/css" /> -->
+        <!-- Daterange picker -->
+        <link href="../css/daterangepicker/daterangepicker-bs3.css" rel="stylesheet" type="text/css" />
+        <!-- iCheck for checkboxes and radio inputs -->
+        <link href="../css/iCheck/all.css" rel="stylesheet" type="text/css" />
+        <!-- bootstrap wysihtml5 - text editor -->
+        <!-- <link href="css/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css" rel="stylesheet" type="text/css" /> -->
+        <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+        <!-- Theme style -->
+        <link href="../css/style.css" rel="stylesheet" type="text/css" />
+
+        <style type="text/css">
+
+        </style>
+    </head>
+
+    <body class="skin-black">
+        <!-- header logo: style can be found in header.less -->
+        <header class="header">
+            <a href="index.php" class="logo">
+                PerpusE-book
+            </a>
+            <!-- Header Navbar: style can be found in header.less -->
+            <nav class="navbar navbar-static-top" role="navigation">
+                <!-- Sidebar toggle button-->
+                <a href="#" class="navbar-btn sidebar-toggle" data-toggle="offcanvas" role="button">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </a>
+                <div class="navbar-right">
+                    <ul class="nav navbar-nav">
+
+                        <!-- User Account: style can be found in dropdown.less -->
+                        <li class="dropdown user user-menu">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="fa fa-user"></i>
+                                <span><?php echo $_SESSION['fullname']; ?> <i class="caret"></i></span>
+                            </a>
+                            <ul class="dropdown-menu dropdown-custom dropdown-menu-right">
+                                <li class="dropdown-header text-center">Account</li>
+
+                                <li>
+                                    <a href="detail-admin.php?hal=edit&kd=<?php echo $_SESSION['user_id']; ?>">
+                                        <i class="fa fa-user fa-fw pull-right"></i>
+                                        Profile
+                                    </a>
+                                    <a href="admin.php">
+                                        <i class="fa fa-cog fa-fw pull-right"></i>
+                                        Settings
+                                    </a>
+                                </li>
+
+                                <li class="divider"></li>
+
+                                <li>
+                                    <a href="../logout.php"><i class="fa fa-ban fa-fw pull-right"></i> Logout</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <?php
+        $timeout = 10; // Set timeout minutes
+        $logout_redirect_url = "../login.html"; // Set logout URL
+
+        $timeout = $timeout * 60; // Converts minutes to seconds
+        if (isset($_SESSION['start_time'])) {
+            $elapsed_time = time() - $_SESSION['start_time'];
+            if ($elapsed_time >= $timeout) {
+                session_destroy();
+                echo "<script>alert('Session Anda Telah Habis!'); window.location = '$logout_redirect_url'</script>";
+            }
+        }
+        $_SESSION['start_time'] = time();
+        ?>
+    <?php } ?>
+    <div class="wrapper row-offcanvas row-offcanvas-left">
+        <!-- Left side column. contains the logo and sidebar -->
+        <aside class="left-side sidebar-offcanvas">
+            <!-- sidebar: style can be found in sidebar.less -->
+            <section class="sidebar">
+                <!-- Sidebar user panel -->
+                <div class="user-panel">
+                    <div>
+                        <center><img src="<?php echo $_SESSION['gambar']; ?>" height="80" width="80" class="img-circle" alt="User Image" style="border: 3px solid white;" /></center>
+                    </div>
+                    <div class="info">
+                        <center>
+                            <p><?php echo $_SESSION['fullname']; ?></p>
+                        </center>
+
+                    </div>
+                </div>
+               
+                <?php include "menu.php"; ?>
+            </section>
+            <!-- /.sidebar -->
+        </aside>
+
+        <aside class="right-side">
+
+            <!-- Main content -->
+            <section class="content">
+
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="panel">
+                            <header class="panel-heading">
+                                <b>Input Admin</b>
+
+                            </header>
+                           
+                            <div class="panel-body">
+                                <form class="form-horizontal style-form" style="margin-top: 20px;" action="insert-admin.php" method="post" enctype="multipart/form-data" name="form1" id="form1">
+                                    <div class="form-group">
+                                        <label class="col-sm-2 col-sm-2 control-label">User ID</label>
+                                        <div class="col-sm-8">
+                                            <input name="user_id" type="text" id="user_id" class="form-control" placeholder="Tidak perlu di isi" autofocus="on" readonly="readonly" />
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 col-sm-2 control-label">Username</label>
+                                        <div class="col-sm-8">
+                                            <input name="username" type="text" id="username" class="form-control" placeholder="Username" required />
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 col-sm-2 control-label">Password</label>
+                                        <div class="col-sm-8">
+                                            <input name="password" type="text" id="password" class="form-control" placeholder="password" required />
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 col-sm-2 control-label">Fullname</label>
+                                        <div class="col-sm-8">
+                                            <input name="fullname" class="form-control" id="fullname" type="text" placeholder="Fullname" required />
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-sm-2 col-sm-2 control-label">Gambar</label>
+                                        <div class="col-sm-8">
+                                            <input name="nama_file" id="nama_file" type="file" />
+                                        </div>
+                                    </div>
+                                    <div class="form-group" style="margin-bottom: 20px;">
+                                        <label class="col-sm-2 col-sm-2 control-label"></label>
+                                        <div class="col-sm-8">
+                                            <input type="submit" value="Simpan" class="btn btn-sm btn-primary" />&nbsp;
+                                            <a href="admin.php" class="btn btn-sm btn-danger">Batal </a>
+                                        </div>
+                                    </div>
+                                    <div style="margin-top: 20px;"></div>
+                                </form>
+                            </div>
+                        </div><!-- /.box -->
+                    </div>
+                </div>
+                <!-- row end -->
+            </section><!-- /.content -->
+           
+        </aside><!-- /.right-side -->
+
+    </div><!-- ./wrapper -->
+
+
+    <!-- jQuery 2.0.2 -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+    <script src="../js/jquery.min.js" type="text/javascript"></script>
+
+    <!-- jQuery UI 1.10.3 -->
+    <script src="../js/jquery-ui-1.10.3.min.js" type="text/javascript"></script>
+    <!-- Bootstrap -->
+    <script src="../js/bootstrap.min.js" type="text/javascript"></script>
+    <!-- daterangepicker -->
+    <script src="../js/plugins/daterangepicker/daterangepicker.js" type="text/javascript"></script>
+
+    <script src="../js/plugins/chart.js" type="text/javascript"></script>
+
+    <script src="../js/plugins/iCheck/icheck.min.js" type="text/javascript"></script>
+    <!-- calendar -->
+    <script src="../js/plugins/fullcalendar/fullcalendar.js" type="text/javascript"></script>
+
+    <!-- Director App -->
+    <script src="../js/Director/app.js" type="text/javascript"></script>
+
+    <!-- Director dashboard demo (This is only for demo purposes) -->
+    <script src="../js/Director/dashboard.js" type="text/javascript"></script>
+
+    <!-- Director for demo purposes -->
+    <script type="text/javascript">
+        $('input').on('ifChecked', function(event) {
+            $(this).parents('li').addClass("task-done");
+            console.log('ok');
+        });
+        $('input').on('ifUnchecked', function(event) {
+            $(this).parents('li').removeClass("task-done");
+            console.log('not');
+        });
+    </script>
+    <script>
+        $('#noti-box').slimScroll({
+            height: '400px',
+            size: '5px',
+            BorderRadius: '5px'
+        });
+
+        $('input[type="checkbox"].flat-grey, input[type="radio"].flat-grey').iCheck({
+            checkboxClass: 'icheckbox_flat-grey',
+            radioClass: 'iradio_flat-grey'
+        });
+    </script>
+    
+    </body>
+
+    </html>
